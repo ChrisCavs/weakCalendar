4f0885733b81cc0d9b7cbfc0ab76f4612d843cd0
ChrisCavs <chris211321@gmail.com>
2018-01-08 17:55:36 -0500

    added timezone

--- resources/js/weakCalendar.js
+++ resources/js/weakCalendar.js
@@ -1,4 +1,6 @@
 setup = function () {
+
+  //retrieve current date info
   let today = new Date();
   let dd = today.getDate();
   let mm = today.getMonth();
@@ -6,9 +8,18 @@ setup = function () {
 
   let monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
+  //add current month to top of page
   let dateMonth = monthArray[mm];
   console.log(dateMonth);
   $(".header-monthof").text(dateMonth);
+
+  //format timezone
+  let splitDate = today.toString().split(' ');
+  let timeZone = splitDate[5].substring(0,6);
+  console.log(timeZone);
+
+  //add timezone to leftside startStop
+  $(".leftside-timezone").text(timeZone);
 }
 
 
