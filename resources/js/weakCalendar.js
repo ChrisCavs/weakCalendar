d101e1af89866ea83febc07804467041e4d1fca6
ChrisCavs <chris211321@gmail.com>
2018-01-08 18:36:11 -0500

    wokring on week

--- resources/js/weakCalendar.js
+++ resources/js/weakCalendar.js
@@ -7,6 +7,9 @@ setup = function () {
   let yyyy = today.getFullYear();
 
   let monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  let weekArray = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+
+  console.log(today);
 
   //add current month to top of page
   let dateMonth = monthArray[mm];
@@ -20,6 +23,13 @@ setup = function () {
 
   //add timezone to leftside startStop
   $(".leftside-timezone").text(timeZone);
+
+  //place current day in week, highlight that day
+  let currentDay = splitDate[0];
+  console.log(currentDay);
+  currentDayTag = "." + currentDay;
+  $(currentDayTag).style('color', '#e6eeff');
+
 }
 
 
