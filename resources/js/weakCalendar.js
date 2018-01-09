c7127695fca2e0eb09c1f7e5dc5cf4254855febf
ChrisCavs <32932937+ChrisCavs@users.noreply.github.com>
2018-01-09 15:50:54 -0500

    removed logs

--- resources/js/weakCalendar.js
+++ resources/js/weakCalendar.js
@@ -9,27 +9,21 @@ function setup () {
   const monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   const weekArray = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 
-  console.log(today);
-
   //add current month to top of page
   const dateMonth = monthArray[mm];
-  console.log(dateMonth);
   document.querySelector('.header-monthof').innerHTML = dateMonth;
 
   //format timezone
   const splitDate = today.toString().split(' ');
   const timeZone = splitDate[5].substring(0,6);
-  console.log(timeZone);
 
   //add timezone to leftside startStop
   document.querySelector('.leftside-timezone').innerHTML = timeZone;
 
   //place current day in week, highlight that day
   const currentDay = splitDate[0];
-  console.log(currentDay);
   currentDayTag = "." + currentDay;
   document.querySelector(currentDayTag).style.color = '#e6eeff';
 };
-
 
 document.addEventListener('DOMContentLoaded', setup);
