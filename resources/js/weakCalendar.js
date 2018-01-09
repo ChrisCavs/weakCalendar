4e8779161d8084dc37560496a440b29ee44e17e3
ChrisCavs <32932937+ChrisCavs@users.noreply.github.com>
2018-01-09 14:45:00 -0500

    updated to vanilla js

--- resources/js/weakCalendar.js
+++ resources/js/weakCalendar.js
@@ -1,10 +1,10 @@
 setup = function () {
 
   //retrieve current date info
-  let today = new Date();
-  let dd = today.getDate();
-  let mm = today.getMonth();
-  let yyyy = today.getFullYear();
+  const today = new Date();
+  const dd = today.getDate();
+  const mm = today.getMonth();
+  const yyyy = today.getFullYear();
 
   let monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   let weekArray = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
@@ -12,27 +12,26 @@ setup = function () {
   console.log(today);
 
   //add current month to top of page
-  let dateMonth = monthArray[mm];
+  const dateMonth = monthArray[mm];
   console.log(dateMonth);
-  $(".header-monthof").text(dateMonth);
+  document.querySelector('.header-monthof').innerHTML(dateMonth);
 
   //format timezone
-  let splitDate = today.toString().split(' ');
-  let timeZone = splitDate[5].substring(0,6);
+  const splitDate = today.toString().split(' ');
+  const timeZone = splitDate[5].substring(0,6);
   console.log(timeZone);
 
   //add timezone to leftside startStop
-  $(".leftside-timezone").text(timeZone);
+  document.querySelector('.leftside-timezone').innerHTML(timeZone);
 
   //place current day in week, highlight that day
-  let currentDay = splitDate[0];
+  const currentDay = splitDate[0];
   console.log(currentDay);
   currentDayTag = "." + currentDay;
-  $(currentDayTag).style('color', '#e6eeff');
-
+  document.querySelector(`${currentDayTag}`).style('color', '#e6eeff');
 }
 
 
-$(document).ready(function () {
+document.ready(function () {
   setup();
 });
