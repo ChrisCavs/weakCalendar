4484c39e25d4ea7cb6fb116cdb5bfb97d58b7a49
ChrisCavs <chris211321@gmail.com>
2018-01-09 20:46:51 -0500

    incomplete attempt at assigning dateDay to full rightside

--- resources/js/weakCalendar.js
+++ resources/js/weakCalendar.js
@@ -7,6 +7,7 @@ function setup () {
   const yyyy = today.getFullYear();
 
   const monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  const monthArrayDay = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
   const weekArray = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 
   //add current month to top of page
@@ -21,9 +22,18 @@ function setup () {
   document.querySelector('.leftside-timezone').innerHTML = timeZone;
 
   //place current day in week, highlight that day
-  const currentDay = splitDate[0];
+  const currentDay = splitDate[0]; // as in mon, tue, wed
+  const dateDay = splitDate[2] // as in 01, 21, 30
   currentDayTag = "." + currentDay;
   document.querySelector(currentDayTag).style.color = '#ff3333';
+
+  //assign date-day to rightside-dates
+  document.querySelector('.rightside-dates span')
+  if (weekArray.indexOf(currentDay) == 0) {
+    document.querySelectorAll('.rightside-dates span').forEach(day => this.innerHTML = dateDay + 
+    })
+  }
+
 };
 
 document.addEventListener('DOMContentLoaded', setup);
