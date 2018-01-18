d15294e8c7669b2f2f0cfad8d02212092bc0eb15
ChrisCavs <32932937+ChrisCavs@users.noreply.github.com>
2018-01-18 15:16:37 -0500

    updated timeArray

--- resources/js/weakCalendar.js
+++ resources/js/weakCalendar.js
@@ -13,7 +13,7 @@ const yyyy = today.getFullYear();
 const monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 const monthArrayDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 const weekArray = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-const timeArray = ['730am', '800am', '830am', '900am', '930am', '1000am', '1030am', '1100am', '1130am', '1200pm', '1230pm', '100pm', '130pm', '200pm', '230pm', '300pm', '330pm', '400pm', '430pm', '500pm', '530pm', '600pm'];
+const timeArray = ['730am', '800am', '830am', '900am', '930am', '1000am', '1030am', '1100am', '1130am', '1200pm', '1230pm', '100pm', '130pm', '200pm', '230pm', '300pm', '330pm', '400pm', '430pm', '500pm', '530pm', '600pm', '630pm', '700pm', '730pm', '800pm', '830pm'];
 
 //globals for rendering page and dates
 let splitDate = today.toString().split(' ');
