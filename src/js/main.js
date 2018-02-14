f8e6e9f94ad4307fc583355d35dda889d5b412e3
Chris <chris211321@gmail.com>
2018-02-14 13:22:33 -0500

    updated styles, working on js setup

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,35 @@
+import {setup} from './setup'
+
 const main = () => {
+
+  let counterWeek = 0
+
+  //retrieve current date info
+  const today = new Date()
+  const dd = today.getDate()
+  const mm = today.getMonth()
+  const yyyy = today.getFullYear()
+
+  //useful arrays for indexing
+  const monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+  const monthArrayDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  const weekArray = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+  const timeArray = ['730am', '800am', '830am', '900am', '930am', '1000am', '1030am', '1100am', '1130am', '1200pm', '1230pm', '100pm', '130pm', '200pm', '230pm', '300pm', '330pm', '400pm', '430pm', '500pm', '530pm', '600pm', '630pm', '700pm', '730pm', '800pm', '830pm']
+
+  //variables for rendering page and dates
+  let splitDate = today.toString().split(' ')
+  let currentDay = splitDate[0] // as in mon, tue, wed
+  let dateDay = splitDate[2] // as in 01, 21, 30
+  let dateMonth = monthArray[mm] // as in february, march, april
+  let dateDayIndex = weekArray.indexOf(currentDay) // as in Mon, Tue, Wed
+
+  //define dateObject to pass to functions
+  let dateObject = {splitDate, currentDay, dateDay, dateMonth, dateDayIndex})
+  console.log(dateObject)
+
+  const timeZone = splitDate[5].substring(0,6)
+
+  setup(timeZone, dateObject)
 
 }
 
---
+++ src/js/setup.js
@@ -0,0 +1,74 @@
+function setup (timeZone, ) {
+
+  //format timezone
+  document.querySelector('.timezone').innerHTML = timeZone;
+
+  //place current day in week, highlight that day if in subheader
+  if (today.toString().split(' ')[2] == dateDay && dateMonth == monthArray[today.getMonth()]) {
+    document.querySelector(currentDayTag).parentElement.style.color = '#ff3333';
+  } else {
+    document.querySelector(currentDayTag).parentElement.style.color = '';
+  }
+
+  //assign dates to subheader + subheader class
+  Array.from(document.querySelectorAll('.date span')).forEach(day => {
+
+    let thisDate = (dateDay*1) + (weekArray.indexOf(day.classList.value) - dateDayIndex);
+    let totalDaysInMonth = monthArrayDays[monthArray.indexOf(dateMonth)];
+
+    //adjust when the day passes the current month
+    if (thisDate > totalDaysInMonth) {
+      thisDate = Math.abs(thisDate - totalDaysInMonth);
+      console.log(thisDate);
+    }
+
+    //adjust when days go negative
+    if (thisDate <= 0) {
+      thisDate = thisDate + totalDaysInMonth;
+    }
+
+    day.innerHTML = thisDate;
+
+    //add class. if class already exists, remove and replace
+    if (day.parentElement.classList.length < 2) {
+      day.parentElement.classList.add(thisDate);
+    } else {
+      day.parentElement.classList.remove(day.parentElement.classList[1]);
+      day.parentElement.classList.add(thisDate);
+    }
+  });
+
+  //add current month to top of page
+  if ((document.querySelectorAll('.date')[1].classList[1])*1 > (document.querySelectorAll('.date')[7].classList[1])*1) {
+    document.querySelector('.header-monthof').innerHTML = `${dateMonth}/${monthArray[monthArray.indexOf(dateMonth) + 1]}`;
+  } else {
+    document.querySelector('.header-monthof').innerHTML = dateMonth;
+  }
+
+  //generate empty rightside divs for content, if they have not already been generated
+  if (document.querySelector('.rightside-column').getElementsByTagName('div').length < 26) {
+    Array.from(document.querySelectorAll('.rightside-column')).forEach(item => {
+      for (var i = 0; i < 26; i++) {
+        let contentPiece = document.createElement('div');
+        contentPiece.className = 'rightside-column-content';
+        item.appendChild(contentPiece);
+      }
+    });
+  }
+
+  //check local storage, fill in data based on saved events
+  checkForData();
+
+  //add event listener for onclick content
+  Array.from(document.querySelectorAll('.rightside-column-content')).forEach(item => item.addEventListener('click', revealModal));
+
+  //add event listeners on buttons
+  document.querySelector('.plus-week').addEventListener('click', addToWeek);
+  document.querySelector('.minus-week').addEventListener('click', subtractFromWeek);
+  document.querySelector('.clear-calendar').addEventListener('click', clearCalendar);
+
+  //listener on header title (to reset to default view)
+  document.querySelector('.header-title').addEventListener('click', defaultView);
+}
+
+export {setup}
